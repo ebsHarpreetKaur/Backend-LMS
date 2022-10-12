f814eb1bdd51a1f90f2c600ecca8f6cd9baf27ba
Prince Kahol <kaholprince4620@gmail.com>
2022-10-12 09:29:35 +0530

    yugjhik

--- app.js
+++ app.js
@@ -1,50 +1,39 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 const cors = require('cors');
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
+const employeeRoute = require("./api/routes/employee");
+const userRoute = require("./api/routes/user");
+const leaveRoute = require("./api/routes/leave");
 
+mongoose.connect(
+  "mongodb+srv://harpreet:123@cluster.2ksky9v.mongodb.net/?retryWrites=true&w=majority"
+);
 
-const employeeRoute = require('./api/routes/employee');
-const userRoute = require('./api/routes/user');
-const leaveRoute = require('./api/routes/leave');
-
-
-
-mongoose.connect('mongodb+srv://harpreet:123@cluster.2ksky9v.mongodb.net/?retryWrites=true&w=majority')
-
-
-mongoose.connection.on('error', err => {
-    console.log('DB connection failed');
+mongoose.connection.on("error", (err) => {
+  console.log("DB connection failed");
 });
 
-mongoose.connection.on('connected', connected => {
-    console.log('Connected with database ');
+mongoose.connection.on("connected", (connected) => {
+  console.log("Connected with database ");
 });
-
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
-
-// API end points 
-app.use('/employee', employeeRoute);
-app.use('/user', userRoute);
-app.use('/leave', leaveRoute);
-
-
-
-
+// API end points
+app.use("/employee", employeeRoute);
+app.use("/user", userRoute);
+app.use("/leave", leaveRoute);
 
 app.use((req, res, next) => {
-    res.status(404).json({
-        error: 'Bad Request'
-    });
-
+  res.status(404).json({
+    error: "Bad Request",
+  });
 });
-
-
 
 module.exports = app;
