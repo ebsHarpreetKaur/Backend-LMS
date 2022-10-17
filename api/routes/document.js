15b6b2d195468368f1377b163a03904dc138b064
Prince Kahol <kaholprince4620@gmail.com>
2022-10-17 16:00:22 +0530

    upload file

--- api/routes/document.js
+++ api/routes/document.js
@@ -3,75 +3,78 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const Document = require('../model/document');
 const checkAuth = require('../middleware/check-auth');
+const upload = require('../middleware/upload')
 // const multer=require('multer')
 // const uploads=multer({dest:'uploads/'})
 
 // Add Document
-router.post('/add',uploads.single('documentfile'),(req,res)=>{
+// router.post('/add',uploads.single('documentfile'),(req,res)=>{
 // router.post('/add',(req,res,next)=>{
-    console.log(req,"jhgkjdfh")
-    console.log(req.files,"jfdghdfkjhkj")''
-    console.log(req.file,"j12345")
-     console.log(req.file)
-     if(!req.file){res.send({code:500,msg:"err"})}
-     else{res.send({code:200,msg:"upload success"})}
-    try {
-        if (!req.files) {
-            res.send({
-                status: "failed",
-                message: "No file uploaded",
-            });
-        } else {
-            let file = req.files.file;
+//     console.log(req,"jhgkjdfh")
+//     console.log(req.files,"jfdghdfkjhkj")
+//     console.log(req.file,"j12345")
+//      console.log(req.file)
+//      if(!req.file){res.send({code:500,msg:"err"})}
+//      else{res.send({code:200,msg:"upload success"})}
+//     try {
+//         if (!req.files) {
+//             res.send({
+//                 status: "failed",
+//                 message: "No file uploaded",
+//             });
+//         } else {
+//             let file = req.files.file;
 
-            console.log(req.files);
+//             console.log(req.files);
 
-            file.mv("./uploads/" + file.name);
+//             file.mv("./uploads/" + file.name);
 
-            res.send({
-                status: "success",
-                message: "File is uploaded",
-                data: {
-                    name: file.name,
-                    mimetype: file.mimetype,
-                    size: file.size,
-                },
-            });
-        }
-    } catch (err) {
-        res.status(500).send(err);
+//             res.send({
+//                 status: "success",
+//                 message: "File is uploaded",
+//                 data: {
+//                     name: file.name,
+//                     mimetype: file.mimetype,
+//                     size: file.size,
+//                 },
+//             });
+//         }
+//     } catch (err) {
+//         res.status(500).send(err);
+//     }
+// })
+
+
+
+
+router.post('/add',upload.single('image'),(req, res, next) => {
+    console.log(req.file,"request file")
+    console.log(req,"Request")
+    const document = new Document({
+        _id: new mongoose.Types.ObjectId,
+        emp_id: req.body.emp_id,
+        documentName: req.body.documentName,
+        documentType: req.body.documentType,
+      
+    }) 
+    if (req.file){
+        document.image = req.file.path 
     }
-})
-
-
-
-
-
-// router.post('/add',(req, res, next) => {
-//     console.log(req.file,"request file")
-//     console.log(req,"Request")
-//     const document = new Document({
-//         _id: new mongoose.Types.ObjectId,
-//         emp_id: req.body.emp_id,
-//         documentName: req.body.documentName,
-//         documentType: req.body.documentType,
-//         file: req.file,
-//     })
    
-//     document.save()
-//         .then(result => {
-//             console.log(result);
-//             res.status(200).json({
-//                 newDocument: result
-//             })
-//         })
-//         .catch(err => {
-//             console.log(err);
-//             res.status(500).json({
-//                 error: err
-//             })
-//         })
-// });
+    document.save()
+        .then(result => {
+            console.log(result,"Result");
+            res.status(200).json({
+                newDocument: result
+            })
+        })
+        .catch(err => {
+            console.log(err,"Error");
+            res.status(500).json({
+                error: err
+            })
+        })
+});
 
 
 
