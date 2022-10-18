b8eb840a85727701d405474824b78bc90ac200c4
Harprittt <ebsharpreetkaur@gmail.com>
2022-10-18 09:10:21 +0530

    .

--- api/routes/document.js
+++ api/routes/document.js
@@ -46,38 +46,34 @@ const upload = require('../middleware/upload')
             //     }
             // })
 
+        
 
 
 
-router.post('/add',upload.single('documentfile'),(req, res, next) => {
-    console.log(req.file,"request file")
-    console.log(req,"Request")
-    const document = new Document({
-        _id: new mongoose.Types.ObjectId,
-        emp_id: req.body.emp_id,
-        documentName: req.body.documentname,
-        documentType: req.body.documenttype,
-      
-    }) 
-    if (req.file){
-        document.documentfile = req.file.path 
-    }
-   
-    document.save()
-        .then(result => {
-            console.log(result,"Result");
-            res.status(200).json({
-                newDocument: result
-            })
 
-        })
-                .catch(err => {
-                    console.log(err);
-                    res.status(500).json({
-                        error: err
-                    })
-                })
-        });
+// router.post('/add', upload.single('image'), (req, res, next) => {
+//     console.log(req.file, "request file")
+//     console.log(req, "Request")
+//     const document = new Document({
+//         _id: new mongoose.Types.ObjectId,
+//         emp_id: req.body.emp_id,
+//         documentName: req.body.documentName,
+//         documentType: req.body.documentType,
+
+//     })
+//     if (req.file) {
+//         document.image = req.file.path
+//     }
+// })
+
+        // })
+        //         .catch(err => {
+        //             console.log(err);
+        //             res.status(500).json({
+        //                 error: err
+        //             })
+        //         })
+        // });
 
 
 
@@ -111,77 +107,77 @@ router.post('/add',upload.single('documentfile'),(req, res, next) => {
 
 
 
-            // get Document by id
-            router.get('/:_id', (req, res, next) => {
-                console.log(req.params._id);
-                Document.findById(req.params._id)
-                    .then(result => {
-                        res.status(200).json({
-                            document: result
-                        })
-                    })
-                    .catch(err => {
-                        console.log(err);
-                        res.status(500).json({
-                            error: err
-                        })
-                    })
-
+// get Document by id
+router.get('/:_id', (req, res, next) => {
+    console.log(req.params._id);
+    Document.findById(req.params._id)
+        .then(result => {
+            res.status(200).json({
+                document: result
             })
-
-
-
-
-
-            // delete Document
-            router.delete('/:_id', (req, res, next) => {
-                Document.remove({ _id: req.params._id })
-                    .then(result => {
-                        res.status(200).json({
-                            message: "Document Deleted",
-                            result: result
-                        })
-                    })
-                    .catch(err => {
-                        res.status(500).json({
-                            error: err
-                        })
-                    })
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
             })
+        })
+
+})
 
 
 
 
 
-            // update/modify Document
-            router.put('/:_id', (req, res, next) => {
-                console.log(req.params._id);
-                console.log(req.body.documentName, "documentName")
-                Document.findOneAndUpdate({ _id: req.params._id }, {
-                    $set: {
-
-                        documentName: req.body.documentName,
-                        documentType: req.body.documentType,
-                        file: req.body.file,
-                    }
-
-                }
-
-                )
-                    .then(result => {
-                        res.status(200).json({
-                            updated_document: result
-                        })
-                    })
-                    .catch(err => {
-                        console.log(err);
-                        res.status(500).json({
-                            error: err
-                        })
-                    })
+// delete Document
+router.delete('/:_id', (req, res, next) => {
+    Document.remove({ _id: req.params._id })
+        .then(result => {
+            res.status(200).json({
+                message: "Document Deleted",
+                result: result
             })
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            })
+        })
+})
 
 
 
 
-            module.exports = router;    
+
+// update/modify Document
+router.put('/:_id', (req, res, next) => {
+    console.log(req.params._id);
+    console.log(req.body.documentName, "documentName")
+    Document.findOneAndUpdate({ _id: req.params._id }, {
+        $set: {
+
+            documentName: req.body.documentName,
+            documentType: req.body.documentType,
+            file: req.body.file,
+        }
+
+    }
+
+    )
+        .then(result => {
+            res.status(200).json({
+                updated_document: result
+            })
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            })
+        })
+})
+
+
+
+
+module.exports = router;    
