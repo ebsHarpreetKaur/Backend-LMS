376b36a686552c6181b7517fb57b9333f3230f45
Baljeet Singh <113023797+Baljeet975@users.noreply.github.com>
2022-10-27 17:56:26 +0530

    change

--- api/model/attendance.js
+++ api/model/attendance.js
@@ -1,17 +1,21 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-attendanceSchema = new mongoose.Schema({
+attendanceSchema = new mongoose.Schema(
+  {
     _id: mongoose.Schema.Types.ObjectId,
-    emp_id: [{
-        type: mongoose.Schema.Types.ObjectId, ref: 'Employee'
-    }],
-   
+    emp_id: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Employee",
+      },
+    ],
+
     CheckIn: String,
     CheckOut: String,
     Break: String,
-    Resume: String
+    Resume: String,
+  },
+  { timestamps: true }
+);
 
-},{ timestamps: true })
-
-
-module.exports = mongoose.model('Attendance', attendanceSchema); 
+module.exports = mongoose.model("Attendance", attendanceSchema);
--- api/routes/attendance.js
+++ api/routes/attendance.js
@@ -1,110 +1,115 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const mongoose = require('mongoose');
-const Attendance = require('../model/attendance');
-const checkAuth = require('../middleware/check-auth');
-
-
+const mongoose = require("mongoose");
+const Attendance = require("../model/attendance");
+const checkAuth = require("../middleware/check-auth");
+const { query } = require("express");
 
 // attendance CheckIn/CheckOut
-router.post('/:emp_id', (req, res, next) => {
-    const attendance = new Attendance({
-        _id: new mongoose.Types.ObjectId,
-        emp_id: req.body.emp_id,
-        CheckIn: req.body.CheckIn,
-        CheckOut: req.body.CheckOut,
-        Break: req.body.Break,
-        Resume: req.body.Resume
-
+router.post("/:emp_id", (req, res, next) => {
+  const attendance = new Attendance({
+    _id: new mongoose.Types.ObjectId(),
+    emp_id: req.body.emp_id,
+    CheckIn: req.body.CheckIn,
+    CheckOut: req.body.CheckOut,
+    Break: req.body.Break,
+    Resume: req.body.Resume,
+  });
+  attendance
+    .save()
+    .then((result) => {
+      console.log(result);
+      res.status(200).json({
+        attendanceRecord: result,
+      });
     })
-    attendance.save()
-        .then(result => {
-            console.log(result);
-            res.status(200).json({
-                attendanceRecord: result
-            })
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json({
-                error: err
-            })
-        })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
 });
-
-
-
-
 
 // Attendance Record Particular Employee
 router.get("/:emp_id", (req, res, next) => {
-    console.log(req.params.emp_id);
-    Attendance.find({ emp_id: req.params.emp_id })
-        .then((result) => {
-            res.status(200).json({
-                attendanceData: result,
-            });
-        })
-        .catch((err) => {
-            console.log(err);
-            res.status(500).json({
-                error: err,
-            });
-        });
+  console.log(req.params.emp_id);
+  Attendance.find({ emp_id: req.params.emp_id })
+    .then((result) => {
+      res.status(200).json({
+        attendanceData: result,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
 });
 
-
-
-
-
-
 //All Employees Attendance Record
-router.get('/', (req, res, next) => {
-    Attendance.find().populate('emp_id')
-        .then(result => {
-            res.status(200).json({
-                attendanceRecord: result
-            });
-        })
-        .catch(err => {
-            console.log(err)
-            res.status(500).json({
-                error: err
-            })
-        });
-})
-
-
-
+router.get("/", (req, res, next) => {
+  Attendance.find()
+    .populate("emp_id")
+    .then((result) => {
+      res.status(200).json({
+        attendanceRecord: result,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
+});
 
 // update employee attendance
 router.put("/:emp_id", (req, res, next) => {
-    console.log(req.params.emp_id);
-    console.log("name", req.body.name);
-    Attendance.findOneAndUpdate(
-        { emp_id: req.params.emp_id },
-        {
-            $set: {
-                CheckIn: req.body.CheckIn,
-                CheckOut: req.body.CheckOut,
-                Break: req.body.Break,
-                Resume: req.body.Resume,
-
-            },
-        }
-    )
-        .then((result) => {
-            res.status(200).json({
-                updatedAttendance: result
-            });
-        })
-        .catch((err) => {
-            console.log(err);
-            res.status(500).json({
-                error: err
-            });
-        });
+  console.log(req.params.emp_id);
+  console.log("name", req.body.name);
+  Attendance.findOneAndUpdate(
+    { emp_id: req.params.emp_id },
+    {
+      $set: {
+        CheckIn: req.body.CheckIn,
+        CheckOut: req.body.CheckOut,
+        Break: req.body.Break,
+        Resume: req.body.Resume,
+      },
+    }
+  )
+    .then((result) => {
+      res.status(200).json({
+        updatedAttendance: result,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
 });
 
+// report monthly;
 
-module.exports = router;  
+router.get("/Month", (req, res, next) => {
+  var query = {
+    CheckIn: {
+      $lte: new Date("2022-10-07"),
+      $gte: new Date("2022-10-09"),
+    },
+  };
+  console.log(query);
+  Attendance.find(query, function (err, data) {
+    if (err) {
+      return res.status(300).json("Error");
+    } else {
+      return res.status(200).json({ data: data });
+    }
+  });
+});
+
+module.exports = router;
