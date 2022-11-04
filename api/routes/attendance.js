b5857044348b4da9feeac05923592c54a5894fb2
Baljeet Singh <113023797+Baljeet975@users.noreply.github.com>
2022-11-04 16:21:11 +0530

    c

--- api/routes/attendance.js
+++ api/routes/attendance.js
@@ -132,13 +132,13 @@ router.get("/record/:emp_id", (req, res, next) => {
 // });
 router.get("/employeeName", (req, res, next) => {
   // console.log(req.body.name,"req.body.name");
-  console.log(req.body,"req.body")
+  console.log(req.body, "req.body");
   Attendance.find({ name: req.body.name })
     .then((result) => {
       res.status(200).json({
         attendanceDataByEmpID: result,
       });
-      console.log(req.body.name,"req.body.name");
+      console.log(req.body.name, "req.body.name");
     })
     .catch((err) => {
       console.log(err);
@@ -147,7 +147,6 @@ router.get("/employeeName", (req, res, next) => {
       });
     });
 });
-
 
 // Employee Attendance Report
 router.post("/", (req, res, next) => {
