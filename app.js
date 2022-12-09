f111a5e63693fb0f4fde984a19c7853111bc7708
Harprittt <ebsharpreetkaur@gmail.com>
2022-12-09 17:19:33 +0530

    changes

--- app.js
+++ app.js
@@ -17,6 +17,8 @@ const leaveRoute = require('./api/routes/leave');
 const documentRoute = require('./api/routes/document');
 const attendanceRoute = require('./api/routes/attendance');
 const holidayRoute = require('./api/routes/holidays');
+const projectRoute = require('./api/routes/project')
+
 // Function to serve all static files
 // inside public directory.
 var publicDir = require('path').join(__dirname, 'api/public');
@@ -58,7 +60,6 @@ const swaggerSpec = swaggerJSDoc(options)
 app.use('/apidocs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 // Swagger Setup
 
-// app.use(express.static(path.join(__dirname, 'uploads')))
 
 // API end points
 app.use("/user", userRoute);
@@ -66,7 +67,9 @@ app.use("/leave", leaveRoute);
 app.use("/document", documentRoute);
 app.use("/attendance", attendanceRoute);
 app.use("/holiday", holidayRoute);
-// app.use('/static', express.static(path.join(__dirname, 'api/uploads')))
+app.use("/project", projectRoute);
+
+
 
 
 
